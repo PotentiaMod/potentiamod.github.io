4ef360015c6dbc7f1e2cbab2d77ffbaede6d45c2
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-13 23:26:35 -0400

    Uploaded the code.

--- Upload to Github.bat
+++ Upload to Github.bat
@@ -4,5 +4,5 @@ git add .
 git commit -m "Uploaded the code."
 git branch -M develop
 git remote add origin https://github.com/PotentiaMod/potentiamod.github.io.git
-git push -f --no-verify origin main
+git push -f --no-verify origin develop
 PAUSE
