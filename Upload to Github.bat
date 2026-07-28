28725d36226983de1e7c7f71dfebd75bf951e878
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-28 01:36:53 -0400

    aaa

--- Upload to Github.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git init
-git add .
-git commit -m "Uploaded the code."
-git branch -M main
-git remote add origin https://github.com/PotentiaMod/potentiamod.github.io.git
-git push -f --no-verify origin main
-PAUSE
