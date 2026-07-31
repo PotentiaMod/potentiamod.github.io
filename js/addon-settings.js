9fe4ca0b34dfbb3d48af1201df2d14812d81ff86
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-31 04:12:30 -0400

    Upload Code

--- js/addon-settings.js
+++ js/addon-settings.js
@@ -149,7 +149,7 @@ var GUI =
 /******/
 /******/
 /******/ 	// add entry module to deferred list
-/******/ 	deferredModules.push(["./src/playground/addon-settings.jsx","vendors~addon-settings~credits~editor~embed~fullscreen~player","addon-settings~credits~editor~embed~fullscreen~player","addon-settings~addons~editor~fullscreen~player"]);
+/******/ 	deferredModules.push(["./src/playground/addon-settings.jsx","vendors~addon-settings~credits~editor~embed~fullscreen~player","addon-settings~credits~editor~embed~fullscreen~player","addon-settings~addons"]);
 /******/ 	// run deferred modules when ready
 /******/ 	return checkDeferredModules();
 /******/ })
